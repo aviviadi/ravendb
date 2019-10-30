e9f64219902b0e2741605f652776a12191756b59
aviviadi <avivi.adi.hr@gmail.com>
2019-10-30 14:15:39 +0200

    RavenDB-13902_nw: merge vfork into latest v4.2

--- src/Sparrow.Server/Platform/Pal.cs
+++ src/Sparrow.Server/Platform/Pal.cs
@@ -2,6 +2,7 @@ using System;
 using System.Buffers;
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
 using System.Text;
 using Sparrow.Platform;
 using Sparrow.Server.Utils;
@@ -11,7 +12,7 @@ namespace Sparrow.Server.Platform
     public static unsafe class Pal
     {
         public static PalDefinitions.SystemInformation SysInfo;
-        public const int PAL_VER = 42012; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
+        public const int PAL_VER = 42013; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
 
         static Pal()
         {
@@ -355,6 +356,28 @@ namespace Sparrow.Server.Platform
 
         [DllImport(LIBRVNPAL, SetLastError = true)]
         public static extern Int64 rvn_get_current_thread_id();
+
+
+        [DllImport(LIBRVNPAL, SetLastError = true)]
+        public static extern PalFlags.FailCodes rvn_spawn_process(
+            string filename,
+            string cmdline,
+            out IntPtr pid,
+            out SafeFileHandle stdin,
+            out SafeFileHandle stdout, // stdout includes redirection of stderr
+            out Int32 errorCode);
+
+        [DllImport(LIBRVNPAL, SetLastError = true)]
+        public static extern PalFlags.FailCodes rvn_wait_for_close_process(
+            IntPtr pid,
+            Int32 closeWaitTimeoutInSeconds,
+            out Int32 exitCode,
+            out Int32 errorCode);
+
+        [DllImport(LIBRVNPAL, SetLastError = true)]
+        public static extern PalFlags.FailCodes rvn_kill_process(
+            IntPtr pid,
+            out Int32 errorCode);
 
         private struct Converter : IDisposable
         {
